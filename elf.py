f37eb525bcfc702a0c72b242bae23439a135b48e
Gialandro <gialandros@gmail.com>
2018-05-11 13:33:55 -0500

    Personal changes...

--- elf.py
+++ elf.py
@@ -1,50 +1,10 @@
-#!/usr/bin/python
-'''
-# Python 2.7.9 script to download EventLogFiles
-# Pre-requisite: standard library functionality = e.g urrlib2, json, StringIO
-
- #/**
- #* Copyright (c) 2012, Salesforce.com, Inc.  All rights reserved.
- #*
- #* Redistribution and use in source and binary forms, with or without
- #* modification, are permitted provided that the following conditions are
- #* met:
- #*
- #*   * Redistributions of source code must retain the above copyright
- #*     notice, this list of conditions and the following disclaimer.
- #*
- #*   * Redistributions in binary form must reproduce the above copyright
- #*     notice, this list of conditions and the following disclaimer in
- #*     the documentation and/or other materials provided with the
- #*     distribution.
- #*
- #*   * Neither the name of Salesforce.com nor the names of its
- #*     contributors may be used to endorse or promote products derived
- #*     from this software without specific prior written permission.
- #*
- #* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- #* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- #* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- #* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- #* HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- #* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- #* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- #* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- #* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- #* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- #* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- #*/
-'''
 # Connected App information: fill it in by creating a connected app
 # https://help.salesforce.com/articleView?id=connected_app_create.htm&language=en_US&type=0
 
 
 #Imports
-
 import urllib.request
-import requests
 import json
-#import ssl
 import getpass
 import os
 import sys
@@ -56,30 +16,34 @@ import base64
 # login function
 def login():
     ''' Login to salesforce service using OAuth2 '''
-    # prompt for username and password
-    username = input('Username: \n')
-    password = getpass.getpass('Password: \n')
-    clientId = input('Consumer key: \n')
-    clientSecret = input('Consumer secret: \n')
+    # username = ''
+    # password = ''
+    # clientId = ''
+    # clientSecret = ''
+    # prompt for username, password, consumer key and consumer secret
+    # while len(username) < 1:
+    username = input('Username: ')
+    # while len(password) < 1:
+    password = getpass.getpass('Password:')
+    # while len(clientId) < 1:
+    clientId = input('Consumer key: ')
+    # while len(clientSecret) < 1:
+    clientSecret = input('Consumer secret: ')
 
     # check to see if anything was entered and if not, default values
     # change default values for username and password to your own
     if len(username) < 1:
-        username = 'your@email.com'
+        username = 'you@mail.com'
         password = 'PasswordToken'
-        clientId = 'YourConsumerKey'
-        clientSecret = 'YourConsumerSecret'
-        # print 'Using default username: ' + username #2.7.9
+        clientId = 'Yourclientid'
+        clientSecret = 'Yourclientsecret'
         print('Using default username and credentials: {0}'.format(username))
     else:
-        # print 'Using user inputed username: ' + username #2.7.9
         print('Using user inputed username and credentials: {0}'.format(username))
 
-    # print 'check point' #2.7.9
     print('check point')
     # create a new salesforce REST API OAuth request
     url = 'https://login.salesforce.com/services/oauth2/token'
-    # dataUnencoded = '&grant_type=password&client_id='+CLIENT_ID+'&client_secret='+CLIENT_SECRET+'&username='+username+'&password='+password #2.7.9
     dataUnencoded = {
         'grant_type': 'password',
         'client_id': clientId,
@@ -90,25 +54,8 @@ def login():
         }
     data = urllib.parse.urlencode(dataUnencoded).encode("utf-8")
     headers = {'X-PrettyPrint' : '1'}
-
-    # workaround to ssl issue introduced before version 2.7.9
-    #if hasattr(ssl, '_create_unverified_context'):
-        #ssl._create_default_https_context = ssl._create_unverified_context
-
-    # These lines are for when you have a proxy server
-    # uncomment the next line to work with a local proxy server. replace the URL with the URL of your proxy
-    # proxy = urllib2.ProxyHandler({'https': 'http://127.0.0.1:8888/'})
-    # uncomment the next two lines if your proxy needs authentication. replace 'realm' through 'password' with appropriate values. 'realm' is often null
-    # proxy_auth_handler = urllib2.HTTPBasicAuthHandler()
-    # proxy_auth_handler.add_password('realm', 'host', 'username', 'password')
-    # pick one of the next two lines based on whether you need proxy authentication. The first is authenticated, the second is unauthenticated
-    # opener = urllib2.build_opener(proxy, proxy_auth_handler)
-    # opener = urllib2.build_opener(proxy)
-    # uncomment the final line to enable the proxy for any calls
-    # urllib2.install_opener(opener)
-
     # call salesforce REST API and pass in OAuth credentials
-    req = urllib.request.Request(url, data, headers)
+    req = urllib.request.Request(url, data, headers = headers)
     # try:
     res = urllib.request.urlopen(req)
     # except urllib.error.URLError as e:
@@ -137,16 +84,14 @@ def download_elf():
     # check to see if anything was entered and if not, default values
     if len(day) < 1:
         day = 'Last_n_Days:2'
-        # print 'Using default date range: ' + day + '\n' #2.7.9
         print('Using default date range: {0} \n'.format(day))
     else:
-        # print 'Using user inputed date range: ' + day + '\n' #2.7.9
         print('Using user inputed date range: {0} \n'.format(day))
 
     # query Ids from Event Log File
     url = instance_url+'/services/data/v41.0/query?q=SELECT+Id+,+EventType+,+Logdate+From+EventLogFile+Where+LogDate+=+'+day
     headers = {'Authorization' : 'Bearer ' + access_token, 'X-PrettyPrint' : '1'}
-    req = urllib.request.Request(url, None, headers)
+    req = urllib.request.Request(url, None, headers = headers)
     res = urllib.request.urlopen(req)
     res_dict = json.load(res)
 
@@ -155,7 +100,6 @@ def download_elf():
 
     # provide feedback if no records are returned
     if total_size < 1:
-        # print 'No records were returned for ' + day #2.7.9
         print('No records were returned for {0}'.format(day))
         sys.exit()
 
@@ -165,10 +109,8 @@ def download_elf():
     # check to see if anything
     if len(dir) < 1:
         dir = 'elf'
-        # print '\ndefault directory name used: ' + dir #2.7.9
         print('\ndefault directory name used: {0}'.format(dir))
     else:
-        # print '\ndirectory name used: ' + dir #2.7.9
         print('\ndirectory name used: {0}'.format(dir))
 
     # If directory doesn't exist, create one
@@ -179,16 +121,16 @@ def download_elf():
     res.close
 
     # check to see if the user wants to download it compressed
-    compress = input('\nUse compression (y/n)\n').lower()
-    print(compress)
+    # compress = input('\nUse compression (y/n)\n').lower()
+    # print(compress)
+    # Disabled compress validation meanwhile...
+    compress = 'n'
 
     # check to see if anything
     if len(compress) < 1:
         compress = 'yes'
-        # print '\ndefault compression being used: ' + compress #2.7.9
         print('\ndefault compression being used: {0}'.format(compress))
     else:
-        # print '\ncompression being used: ' + compress #2.7.9
         print('\ncompression being used: {0}'.format(compress))
 
     # loop over json elements in result and download each file locally
@@ -204,11 +146,9 @@ def download_elf():
         # provide correct compression header
         if (compress == 'y') or (compress == 'yes'):
             headers = {'Authorization' : 'Bearer ' + access_token, 'X-PrettyPrint' : '1', 'Accept-encoding' : 'gzip'}
-            # print 'Using gzip compression\n' #2.7.9
             print('Using gzip compression\n')
         else:
             headers = {'Authorization' : 'Bearer ' + access_token, 'X-PrettyPrint' : '1'}
-            # print 'Not using gzip compression\n' #2.7.9
             print('Not using gzip compression\n')
 
         # begin profiling
@@ -218,11 +158,9 @@ def download_elf():
         req = urllib.request.Request(url, None, headers)
         res = urllib.request.urlopen(req)
 
-        # print '********************************' #2.7.9
         print('********************************')
 
         # provide feedback to user
-        # print 'Downloading: ' + dates[:10] + '-' + types + '.csv to ' + os.getcwd() + '/' + dir + '\n' #2.7.9
         print('Downloading: ' + dates[:10] + '-' + types + '.csv to ' + os.getcwd() + '/' + dir + '\n')
 
         # print the response to see the content type
@@ -232,7 +170,11 @@ def download_elf():
         # compression code from http://bit.ly/pyCompression
         if res.info().get('Content-Encoding') == 'gzip':
             # buffer results
-            buf = StringIO(res.read().decode('utf-8'))
+            html = res.read()
+            decodedHtml = html.decode('iso-8859-1')#.encode('UTF-8')
+            # decodedHtml = decodedHtml.decode('UTF-8')
+            print('HTML=>', decodedHtml)
+            buf = StringIO(decodedHtml)
             # gzip decode the response
             f = gzip.GzipFile(fileobj=buf)
             # print data
@@ -257,7 +199,6 @@ def download_elf():
 
         #msecs = secs * 1000  # millisecs
         #print 'elapsed time: %f ms' % msecs
-        # print 'Total download time: %f seconds\n' % secs #2.7.9
         print('Total download time: %f seconds\n' % secs)
 
         file.close
